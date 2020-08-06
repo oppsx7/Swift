5b304300ec7b745f81cbce4b5cb5138f6b909439
todor.dimitrov <40122992+oppsx7@users.noreply.github.com>
2020-08-06 10:05:50 +0300

    Add files via upload

---
+++ Project5/Project5/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Project5
+//
+//  Created by Todor Dimitrov on 6.08.20.
+//  Copyright © 2020 Todor Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var allWords = [String]()
+    var usedWords = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty {
+            allWords = ["silkworm"]
+        }
+        
+        startGame()
+    }
+    
+    func startGame() {
+        title = allWords.randomElement()
+        usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        cell.textLabel?.text = usedWords[indexPath.row]
+        return cell
+    }
+
+}
+
