c4f9434a4895e3b8000ee1b7f4083b934f54f07e
todor.dimitrov <40122992+oppsx7@users.noreply.github.com>
2020-08-06 09:59:32 +0300

    Add files via upload

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Todor Dimitrov on 4.08.20.
+//  Copyright © 2020 Todor Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                    //this is a picture to load !
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.sort(by: <)
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 20)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.rowNumber = pictures.count
+            vc.currentRow = indexPath.row + 1
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
+
