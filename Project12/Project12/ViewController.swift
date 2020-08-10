58765070e5428c37caa06a86418ce2dd0b2caeee
Todor Dimitrov <todordimitrov@Mac-mini-2.local>
2020-08-10 17:23:34 +0300

    Project 10 Encode/Decode using NSCoding and Codable

---
+++ Project12/Project12/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Project12
+//
+//  Created by Todor Dimitrov on 10.08.20.
+//  Copyright © 2020 Todor Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolean = defaults.bool(forKey: "UseFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
+        
+    }
+
+
+}
+
---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,29 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Todor Dimitrov on 10.08.20.
+//  Copyright © 2020 Todor Dimitrov. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+}
