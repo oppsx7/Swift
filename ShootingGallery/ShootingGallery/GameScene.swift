f29fa4429e6a31281d731f868f8f0565dc70e622
Todor Dimitrov <todordimitrov@Mac-mini-2.local>
2020-08-12 16:33:41 +0300

    TODO Shooting gallery D66

--- ShootingGallery/ShootingGallery/GameScene.swift
+++ ShootingGallery/ShootingGallery/GameScene.swift
@@ -19,27 +19,6 @@ class GameScene: SKScene {
     
     
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        <#code#>
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
